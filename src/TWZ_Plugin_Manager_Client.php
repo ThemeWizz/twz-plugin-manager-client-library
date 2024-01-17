0d60e23755b80b18965c2241070c4c8aa13a17cd
Sergey Tulyaganov <st@sersart.com>
2024-01-17 07:29:40 -0500

    Fixed bugs

---
+++ src/TWZ_Plugin_Manager_Client.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace themewizz\twz_plugin_manager;
+
+use InvalidArgumentException;
+
+class TWZ_Plugin_Manager_Client
+{
+	private $server_url = '';
+	private $plugin_slug = '';
+
+	private $show_type = 'our_products';
+	protected $plugins = null;
+	protected $categories = 'all';
+
+	public function __construct($server_url, $plugin_slug)
+	{
+		if (!$server_url) {
+			throw new InvalidArgumentException("Missing config: [server_url]");
+		}
+		$this->server_url = $server_url;
+
+		if (!$plugin_slug) {
+			throw new InvalidArgumentException("Missing config: [plugin_slug]");
+		}
+		$this->plugin_slug = $plugin_slug;
+
+		load_plugin_textdomain('twz-plugin-manager-client', false, dirname(dirname(plugin_basename(__FILE__))) . '/languages/');
+	}
+
+	public function show_addons()
+	{
+		$this->show_type = 'addons';
+		$this->init();
+	}
+
+	public function show_products()
+	{
+		$this->show_type = 'our_products';
+		$this->init();
+	}
+
+	protected function init()
+	{
+		wp_enqueue_style('plugin-installer.css',  plugin_dir_url(__FILE__) . 'assets/css/installer.css', array(), '1.0.0');
+		include('views/products-page.php');
+		return true;
+	}
+
+	public function prepare_items()
+	{
+		$nonce = wp_create_nonce('_wpnonce_the-wizz-memberships-addons-ajax');
+		$url = $this->server_url . '/wp-admin/admin-ajax.php?XDEBUG_SESSION_START=PHPSTORM';
+		$raw_response = wp_remote_post(
+			$url,
+			array(
+				'method'      => 'POST',
+				'redirection' => 5,
+				'timeout' => 5000,
+				'headers'     => array(
+					'X-WP-Nonce' => $nonce
+				),
+				'body' => array(
+					'action' => 'the_wizz_memberships_addons_list',
+					'nonce' => wp_create_nonce('twz-plugin-manager-client'),
+					'type' => $this->show_type,
+					'slug' => $this->plugin_slug
+				),
+			)
+		);
+
+		if (!is_wp_error($raw_response) && 200 == $raw_response['response']['code']) {
+			$this->plugins = json_decode($raw_response['body'], true);
+		} else {
+			if (!empty($raw_response['body'])) {
+				echo $raw_response['body'];
+				exit;
+			}
+			$this->plugins = array();
+		}
+	}
+}
--- src/views/products-page.php
+++ src/views/products-page.php
@@ -1,63 +1,25 @@
 <?php $this->prepare_items(); ?>
 
 <div class="twz-quiz-plugin-installer">
-    <?php foreach ($this->plugins as $plugin) {
-        $button_classes = 'install button';
-        $button_text    = __('Install Now');
-
-        $main_plugin_file = $this->get_plugin_file($plugin['twz_plugin_slug']);
-
-        if ($this->check_file_extension($main_plugin_file)) {
-            if (is_plugin_active($main_plugin_file)) {
-                // plugin activated.
-                $button_classes = 'button disabled';
-                $button_text    = __('Activated', 'twz-plugin-manager-client');
-            } else {
-                // Installed, let's activate it.
-                $button_classes = 'activate btn btn-primary btn-sm';
-                $button_text    = __('Activate', 'twz-plugin-manager-client');
-            }
-        }
-        $twz_addons_groups = [];
-        if (!empty($plugin['twz_addons_groups'])) {
-            $twz_addons_groups = json_decode($plugin['twz_addons_groups']);
-        }
-    ?>
+    <?php foreach ($this->plugins as $plugin) { ?>
         <div class="plugin">
-            <div class="d-flex" style="height: calc(100% - 107px);">
-                <div class="p-2">
-                    <?php if ($plugin['twz_plugin_icon']) { ?>
-                        <img style="width: 100px;" src="<?php echo esc_attr($plugin['twz_plugin_icon']); ?>" alt="">
-                    <?php } else { ?>
-                        <img style="width: 100px;" src="https://s.w.org/plugins/geopattern-icon/classic-widgets.svg" alt="">
-                    <?php } ?>
+            <div style="display:flex;">
+                <div style="padding: 0.5rem!important;">
+                    <img style="width: 100px;" src="<?php echo esc_attr($plugin['twz_plugin_image']); ?>" alt="">
                 </div>
-                <div class="p-2">
-                    <h2><?php echo wp_kses_post($plugin['twz_plugin_title']); ?></h2>
+                <div style="padding: 0.5rem!important;">
+                    <div class="plugin-title"><?php echo wp_kses_post($plugin['twz_plugin_title']); ?></div>
                     <p><?php echo wp_kses_post($plugin['twz_plugin_description']); ?></p>
-                    <p class="plugin-author"><?php esc_attr_e('By', 'twz-plugin-manager-client'); ?> <?php echo wp_kses_post($plugin['twz_plugin_author']); ?></p>
+                    <p class="plugin-author"><?php esc_attr_e('By', 'twz-plugin-manager-client'); ?> <b><?php echo wp_kses_post($plugin['twz_plugin_author']); ?></b></p>
                 </div>
             </div>
-            <div class="d-flex justify-content-center" style="height: 35px; color: green;">
-                <?php foreach ($twz_addons_groups as $groups) {
-
-                ?>
-                    <p class="m-2"><?php echo (ucfirst($groups)); ?></p>
-
-                <?php } ?>
-            </div>
-            <ul class="activation-row">
-                <li>
-                    <a class="<?php echo esc_attr($button_classes); ?>" data-slug="<?php echo esc_attr($plugin['twz_plugin_slug']); ?>" data-name="<?php echo esc_attr($plugin['twz_plugin_title']); ?>" href="#">
-                        <?php echo esc_attr($button_text); ?>
-                    </a>
-                </li>
-                <li>
-                    <a href="<?php echo esc_attr($plugin['twz_plugin_help_url']); ?>/" target="_blank">
+            <div class="activation-row">
+                <div class="custom-button">
+                    <a class="custom-link" href="<?php echo esc_attr($plugin['twz_plugin_help_url']); ?>/" target="_blank">
                         <?php esc_attr_e('More Details', 'twz-plugin-manager-client'); ?>
                     </a>
-                </li>
-            </ul>
+                </div>
+            </div>
         </div>
     <?php } ?>
 </div>
